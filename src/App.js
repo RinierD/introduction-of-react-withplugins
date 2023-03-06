d43179237995994f175482f2cc2cb486861077c6
Rinier Dapugo <rinierdapugo@gmail.com>
2023-03-06 10:31:10 +0800

    Adding CSS to react profect also using 'useEffect' to control the render of the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-import { useState } from "react";
-import Button from "./Button";
+import { useEffect, useState } from "react";
+// import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  const [] = useState();
+  // const iRunOnlyOnce = () => {
+  //   console.log("I run once");
+  // };
+  // useEffect(iRunOnlyOnce, []);
+
+  // useEffect(() => {
+  //   console.log("Call the API");
+  // }, []);
+
+  useEffect(() => {
+    console.log("Only run once");
+  }, []);
+
+  useEffect(() => {
+    console.log("Only run once keyword is change");
+  }, [keyword]);
+
+  useEffect(() => {
+    console.log("Only run once counter is change");
+  }, [counter]);
+
   return (
     <div>
-      <h1>Hi! Welcome Back</h1>
-      <Button text={"Click Me"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search Here..."></input>
+      <h1 className={styles.title}>Hi! Welcome Back</h1>
+      <h1>{counter}</h1>
+      <button onClick={onClick}> Click to Count </button>
+      {/* <Button text={"Click Me"} onClick={onClick} /> */}
     </div>
   );
 }
--- src/Button.js
+++ src/Button.js
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import BtnStyle from "./Button.module.css";
 
 function Button({ text }) {
-  return <button className="BtnStyle">{text}</button>;
+  return <button className={BtnStyle.button}>{text}</button>;
 }
 
 Button.propTypes = {
